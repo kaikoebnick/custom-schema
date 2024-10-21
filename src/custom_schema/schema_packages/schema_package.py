e96ed398a197ea03b466e7786943007d8f16c644
kaikoebnick <kai.koebnick@gmail.com>
2024-10-21 20:32:54 +0200

    Update schema_package.py

--- src/custom_schema/schema_packages/schema_package.py
+++ src/custom_schema/schema_packages/schema_package.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
     )
     from structlog.stdlib import (
         BoundLogger,
-    ) """
+    ) 
 
 from nomad.config import config
 from nomad.datamodel.data import (
@@ -84,11 +84,234 @@ class NewSchemaPackage(Schema):
         a_eln=ELNAnnotation(default=f'{date_and_time}--{measurement_name}'),
     )
 
-    """def normalize(self, archive, logger):
-        super(Schema, self).normalize(archive, logger)
 
-        if self.sample_id is None:
-            self.sample_id = f'{self.date_and_time}--{self.measurement_name}'"""
+m_package.__init_metainfo__()"""
+
+
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from nomad.datamodel.datamodel import EntryArchive
+    from structlog.stdlib import BoundLogger
+
+import xml
+
+from nomad.datamodel.data import (
+    ArchiveSection,
+    EntryDataCategory,
+    Schema,
+)
+from nomad.datamodel.data import Author as NomadAuthor
+from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
+from nomad.metainfo import (
+    Category,
+    Datetime,
+    #MEnum,
+    Quantity,
+    SchemaPackage,
+    Section,
+    SubSection,
+)
+
+m_package = SchemaPackage(name='laserphysics ELN schema')
+
+
+class ToolsCategory(EntryDataCategory):
+    m_def = Category(label='Basic ELN', categories=[EntryDataCategory])
+
+
+def remove_tags(text):
+    return ''.join(xml.etree.ElementTree.fromstring(text).itertext())
+
+
+class Material(ArchiveSection):
+    m_def = Section(a_eln=ELNAnnotation(overview=True))
+
+    name = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
+        description='Name of the material.',
+    )
+    formula = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
+        description='Chemical formula of the material.',
+    )
+
+
+"""class System(ArchiveSection):
+    m_def = Section(a_eln=ELNAnnotation(overview=True))
+
+    name = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
+        description='Specifying name of the system.',
+    )"""
+
+
+class Author(ArchiveSection):
+    m_def = Section(a_eln=ELNAnnotation(overview=True))
+
+    first_name = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.StringEditQuantity, label='First Name'
+        ),
+        description='First name of the author',
+    )
+
+    last_name = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.StringEditQuantity, label='Last Name'
+        ),
+        description='Last name of the author.',
+    )
+
+
+class Reference(ArchiveSection):
+    m_def = Section(a_eln=ELNAnnotation(overview=True))
+
+    kind = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.EnumEditQuantity,
+            props=dict(
+                suggestions=[
+                    'article url',
+                    'dataset url',
+                    'video url',
+                    'picture url',
+                    'documentation',
+                    'other',
+                ]
+            ),
+        ),
+    )
+
+    name = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
+        description='Human readable name for the reference.',
+    )
+
+    description = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.RichTextEditQuantity),
+        description='Extra details about the reference.',
+    )
+
+    uri = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.URLEditQuantity, label='URI'),
+        description='External URI for the reference.',
+    )
+
+    version = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
+        description='Optional field for adding version information.',
+    )
+
+
+class laserphysicsELN(Schema):
+    m_def = Section(
+        label='laserphysics ELN schema',
+        categories=[ToolsCategory],
+        a_eln=ELNAnnotation(),
+    )
+
+    name = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
+        label='Name/Title',
+        description='Short name of the ELN.',
+    )
+
+    date = Quantity(
+        type=Datetime,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.DateEditQuantity),
+        label='Last update',
+        description='The date of the last update.',
+    )
+
+    description = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(component=ELNComponentEnum.RichTextEditQuantity),
+        description='Short description of the ELN. You can add pictures!',
+    )
+
+    category = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.EnumEditQuantity,
+            props=dict(
+                suggestions=[
+                    'measurement',
+                    'calibration',
+                    'other',
+                ]
+            ),
+        ),
+    )
+
+    tag_1 = Quantity(
+        type=bool,
+        description='''Check if there is a nice peak in tag_1.''',
+        a_eln=ELNAnnotation(component=ELNComponentEnum.BoolEditQuantity)
+    )
+
+    voltage = Quantity(
+        type=float,
+        description='''Enter your voltage.''',
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+        )
+    )
+    number_of_people_killed_during_measurement = Quantity(
+        type=int,
+        description='''Let us hope it is zero.''',
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.SliderEditQuantity,
+            minValue=0,
+            maxValue=50,
+        )
+    )
+
+    """platform = Quantity(
+        type=MEnum(['Python', 'Julia', 'R', 'other']),
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.AutocompleteEditQuantity,
+        ),
+    )"""
+
+    authors = SubSection(section=Author, repeats=True)
+
+    material = SubSection(section=Material, repeats=True)
+
+#    systems = SubSection(section=System, repeats=True)
+
+    references = SubSection(section=Reference, repeats=True)
+
+    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
+        super().normalize(archive, logger)
+
+        if self.name:
+            archive.metadata.entry_name = self.name
+
+        if self.description:
+            if self.description.startswith('<'):
+                comment = remove_tags(self.description)
+            else:
+                comment = self.description
+
+            archive.metadata.comment = comment
+
+        if self.authors:
+            archive.metadata.entry_coauthors = [
+                NomadAuthor(**author.m_to_dict()) for author in self.authors
+            ]
 
 
 m_package.__init_metainfo__()
