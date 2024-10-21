bf3f910e105b92c6204b2ff919a943e71a7c48c8
kaikoebnick <kai.koebnick@gmail.com>
2024-10-21 20:30:56 +0200

    Update __init__.py

--- src/custom_schema/__init__.py
+++ src/custom_schema/__init__.py
@@ -1,14 +1,1 @@
-from nomad.config.models.plugins import SchemaPackageEntryPoint
 
-
-class custom_schemaPackageEntryPoint(SchemaPackageEntryPoint):
-    def load(self):
-        from custom_schema.schema_packages import m_package
-
-        return m_package
-
-
-custom_schema = custom_schemaPackageEntryPoint(
-    name='Laserphysics ELN',
-    description='Describes the basic schemas for laserphysics notebooks.',
-)
